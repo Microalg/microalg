442affa07fd1358d66a1919d339f706aaf03031a
Christophe Gragnic <christophegragnic@gmail.com>
2014-04-26 22:48:36 +0200

    Première version de l’IDE web.

---
+++ web_ide/ide_injections.js
@@ -0,0 +1,80 @@
+function onCtrlEnter(elt, f) {
+    elt.keydown(function (e) {
+        if ((e.keyCode == 10 || e.keyCode == 13) && e.ctrlKey) {
+            f(elt);
+        }
+    });
+}
+
+function inject_microalg_editor_in(selector, msg) {
+    var script_container = $(selector);
+    var script_string = '<textarea id="malg-editor" class="malg-editor" cols="80" style="width:98%;" rows="5" >' + msg + '</textarea>' +
+            '<div class="malg-error" style="color: red;"></div>' +
+            '<div class="malg-display">&nbsp;</div>';
+    script_container.html(script_string);
+    var editor = script_container.find('.malg-editor').first();
+    // Load local storage in the editor.
+    if (typeof(Storage)!=="undefined") {
+        if (localStorage.microalg_src) {
+            editor.val(localStorage.microalg_src);
+        } else {
+            editor.val('(Afficher "MicroAlg FTW!")');
+        }
+    }
+    createRichInput(editor);
+    editor.focus();
+    onCtrlEnter(editor, ide_action);
+    function ide_action(editor_elt) {
+        var src = editor_elt.val();
+        // createRichInput put the editor in a sub div, that's why we use
+        // parent().parent()
+        var error_elt = editor_elt.parent().parent().find('.malg-error').first();
+        var display_elt = editor_elt.parent().parent().find('.malg-display').first();
+        display_elt.html('&nbsp;');
+        try {
+            EMULISP_CORE.eval(src).toString();
+            error_elt.text('');
+        } catch(e) {
+            error_elt.text(e.toString());
+        }
+        var stdout = EMULISP_CORE.currentState().iSym['*StdOut'].cdr.name;
+        if (stdout == '') {
+            stdout = '&nbsp;';
+        }
+        display_elt.html(stdout);
+        EMULISP_CORE.currentState().iSym['*StdOut'].cdr.name = '';
+        localStorage.microalg_src = src;
+    }
+}
+
+function inject_microalg_repl_in(selector, msg) {
+    var malg_prompt = ": ";
+    var repl_container = $(selector);
+    var repl_string = '<textarea id="malg-repl" class="malg-repl" style="width:98%;" rows="5" >' + malg_prompt + msg + '</textarea>' +
+        '<p>Besoin d’aide&nbsp;? Taper «&nbsp;(Aide)&nbsp;» puis <code>Ctrl</code>+<code>Entrée</code> pour valider.</p>';
+    repl_container.html(repl_string);
+    var repl = repl_container.find('.malg-repl').first();
+    createRichInput(repl);
+    onCtrlEnter(repl, repl_action);
+    var old_src = malg_prompt;
+    function repl_action(repl_elt) {
+        var result = '';
+        var repl_content = repl_elt.val();
+        var src = repl_content.slice(old_src.length, repl_content.length);
+        try {
+            result = EMULISP_CORE.eval(src).toString();
+        } catch(e) {
+            repl_elt.val(repl_elt.val() + "\n" + e.toString());
+        }
+        if (result != '' && result != 'NIL') {
+            repl_elt.val(repl_elt.val() + "\n-> " + result);
+        }
+        var stdout = EMULISP_CORE.currentState().iSym['*StdOut'].cdr.name;
+        if (stdout != '' && stdout != 'NIL') {
+            repl_elt.val(repl_elt.val() + "\n" + stdout);
+        }
+        repl_elt.val(repl_elt.val() + "\n" + malg_prompt);
+        EMULISP_CORE.currentState().iSym['*StdOut'].cdr.name = '';
+        old_src = repl_elt.val();
+    }
+}
---
+++ web_ide/parenedit.js
@@ -0,0 +1,114 @@
+// http://stackoverflow.com/questions/14043727/auto-resizing-textarea-is-bumpy-highlighting-parentheses
+// http://jsfiddle.net/ACF8e/
+
+function getDefaultFontSize(pa){
+     pa = pa || document.body;
+     var who= document.createElement('span');
+     who.className= 'defaultEm';
+     who.appendChild(document.createTextNode('M'));
+     pa.appendChild(who);
+     var fs= [who.offsetWidth, who.offsetHeight];
+     pa.removeChild(who);
+     return fs;
+}
+// Elt in parameter needs an id.
+function createRichInput(original) {
+    if (typeof original == "string") original = $(original);
+    var newId = "richtext_" + original.attr('id');
+    var newElement = "<div class=\"richtext\" id=\"" + newId + "\"></div>";
+
+    newId = '#' + newId;
+
+    var width = original.width();
+    var height = original.height();
+
+    original.wrap(newElement);
+    original.before("<pre></pre>");
+
+    $(newId).width(width).css("min-height", height);
+    $(newId + '> pre').width(width).css("min-height", height);
+
+    original.css({
+        'background': 'transparent',
+        'position': 'absolute',
+        'z-index': 100,
+        'width': width
+    });
+
+    original.bind('propertychange keydown keyup input paste click', function(e) {
+        var code = (e.keyCode ? e.keyCode : e.which);
+        var text = original.val();
+        if (code == 13) text += '<br>';
+        $(newId + " pre").html(colorize(text, original.getCursorPosition()));
+
+        var newHeight = $(newId + " pre").height() + getDefaultFontSize()[1]; //adding a height of line
+        $(this).height(newHeight);
+        $(newId).height(newHeight);
+    });
+    original.trigger(jQuery.Event("keydown"));
+}
+
+function colorize(text, pos) {
+    var i = 0, current_times = 0;
+    var startc = '(', endc = ')';
+    var current = -1;
+
+    var entities = {'>': '&gt;','<':'&lt;'};
+    var p2 = 0;
+    var regex = new RegExp(Object.keys(entities).join("|"),'g');
+    var converted = text.replace(regex, function(x, j) {
+        if(pos > j) p2 += entities[x].length - 1;
+        return entities[x];
+    });
+
+    pos += p2;
+    var parens = [], indices = [], o = {};
+    var newText = converted.replace(/((?:\\)*)([()])/g, function(full, escape, x, idx) {
+        var len = escape.split(/\\/g).length - 1;
+        if (len % 2 == 0) {
+            indices.push(idx);
+            if (x == startc) ++i;
+            o[idx] = { selected: false, type: x, depth: i, idx: idx, pair: -1, extra: escape };
+            if (idx == pos) o[idx].selected = true;
+            if (x == startc) parens.push(idx);
+            else {
+                if (parens.length > 0) {
+                    var p = parens.pop();
+                    o[idx].pair = p;
+                    if (o[p].selected) o[idx].selected = true;
+                    o[p].pair = idx;
+                    if (o[idx].selected) o[p].selected = true;
+                }
+                --i
+            }
+        }
+    });
+    newtext = converted;
+    indices = indices.sort(function(x,y) { return Number(y) - Number(x); });
+    indices.forEach(function(i) {
+        newtext = newtext.substr(0,i) + o[i].extra +
+        "<span class='" + (o[i].pair == -1 ? "unmatched " : "paren_" + (o[i].depth % 6)) +
+        (o[i].selected ? " selected_paren": "") + "'>" + o[i].type + "</span>" +
+        newtext.substr(i + 1 + o[i].extra.length)
+    });
+    return newtext;
+}
+
+(function($) {
+    $.fn.getCursorPosition = function() {
+        var input = this.get(0);
+        if (!input) return; // No (input) element found
+        if ('selectionStart' in input) {
+            // Standard-compliant browsers
+            return input.selectionStart;
+        } else if (document.selection) {
+            // IE
+            input.focus();
+            var sel = document.selection.createRange();
+            var selLen = document.selection.createRange().text.length;
+            sel.moveStart('character', -input.value.length);
+            return sel.text.length - selLen;
+        }
+    }
+})(jQuery);
+
