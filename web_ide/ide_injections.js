b042c3fb27ad8a9c8b49ea1456bde558fbaab282
Christophe Gragnic <christophegragnic@gmail.com>
2014-04-27 12:32:38 +0200

    REPL: Move html output to a modal.

--- web_ide/ide_injections.js
+++ web_ide/ide_injections.js
@@ -50,8 +50,7 @@ function inject_microalg_editor_in(selector, msg) {
 function inject_microalg_repl_in(selector, msg) {
     var malg_prompt = ": ";
     var repl_container = $(selector);
-    var repl_string = '<div class="malg-repl-html-out"></div>' +
-        '<textarea id="malg-repl" class="malg-repl" style="width:98%;" rows="5" >' + malg_prompt + msg + '</textarea>' +
+    var repl_string = '<textarea class="malg-repl" style="width:98%;" rows="5" >' + malg_prompt + msg + '</textarea>' +
         '<p>Besoin d’aide&nbsp;? Taper «&nbsp;(Aide)&nbsp;» puis <code>Ctrl</code>+<code>Entrée</code> pour valider.</p>';
     repl_container.html(repl_string);
     var repl = repl_container.find('.malg-repl').first();
@@ -62,7 +61,6 @@ function inject_microalg_repl_in(selector, msg) {
         var result = '';
         var repl_content = repl_elt.val();
         var src = repl_content.slice(old_src.length, repl_content.length);
-        var html_out = repl_elt.parent().parent().find('.malg-repl-html-out').first();
         try {
             result = EMULISP_CORE.eval(src).toString();
         } catch(e) {
@@ -70,7 +68,7 @@ function inject_microalg_repl_in(selector, msg) {
         }
         if (result != '' && result != 'NIL') {
             repl_elt.val(repl_elt.val() + "\n-> " + result);
-            html_out.html(result);
+            $.modal(result, {onClose: function (dialog) {$.modal.close();repl_elt.focus();}});
         }
         var stdout = EMULISP_CORE.currentState().iSym['*StdOut'].cdr.name;
         if (stdout != '' && stdout != 'NIL') {
