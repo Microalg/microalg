cb46a911788761241a2c8d34ae15b0bb943f9512
Christophe Gragnic <christophegragnic@gmail.com>
2014-05-04 09:16:22 +0200

    features/cli.py: implement a timeout guard.

--- features/cli.py
+++ features/cli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from lettuce import world, step, before
 from subprocess import Popen, PIPE
+import signal
 
 @before.each_scenario
 def setup_some_scenario(scenario):
@@ -9,6 +10,12 @@ def setup_some_scenario(scenario):
     world.inputs = []     # list of strings that the user may input
     world.output = ''     # string that may be printed
     world.error = ''      # errors that may occur
+
+class TimeoutException(Exception):
+    pass
+
+def timeout_handler(signum, frame):
+    raise TimeoutException()
 
 @step(u'Le programme (.*)')
 def le_programme(step, interp):
@@ -28,8 +35,17 @@ def avec_interaction(step, user_input):
 
 @step(u'Doit afficher «(.*)»')
 def doit_afficher(step, expected):
+    timeout = 1
+    signal.signal(signal.SIGALRM, timeout_handler)
+    signal.alarm(timeout)  # timeout in seconds
     input_str = world.inputs and '\n'.join(world.inputs) or None
-    world.output, world.error = world.process.communicate(input_str)
+    try:
+        world.output, world.error = world.process.communicate(input_str)
+        signal.signal(signal.SIGALRM, signal.SIG_DFL)
+    except TimeoutException:
+        world.process.kill()
+        tpl = "L’exécution aurait du durer moins de %d seconde(s)."
+        raise AssertionError(tpl % timeout)
     if world.output != expected:
         tpl = "Devait afficher %s, mais on a eu: %s."
         raise AssertionError(tpl % (expected, world.output))
