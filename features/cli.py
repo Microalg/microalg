c08713ad1a9f79a8b029e947dd9f7bc4430c055b
Christophe Gragnic <christophegragnic@gmail.com>
2014-05-05 21:54:12 +0200

    features/cli.py: catch KeybInterrupt and pexpect.{EOF,TIMEOUT}.

--- features/cli.py
+++ features/cli.py
@@ -7,6 +7,7 @@ def world_init(scenario):
     world.interp = ''     # name of the interpreter
     world.arg_list = []   # list of strings to provide to the interpreter
     world.process = None  # the process pexpect will spawn
+    world.timeout = 5     # timeout in seconds
     world.inputs = []     # list of strings that the user may input
     world.output = ''     # string that will be displayed
 
@@ -20,7 +21,8 @@ def avec_argument(step, arg):
 
 @step(u'Ayant démarré')
 def ayant_demarre(step):
-    world.process = pexpect.spawn(world.interp, world.arg_list, timeout=5)
+    world.process = pexpect.spawn(world.interp, world.arg_list,
+                                  timeout=world.timeout)
 
 @step(u'Avec interaction (.*)')
 def avec_interaction(step, user_input):
@@ -28,19 +30,38 @@ def avec_interaction(step, user_input):
 
 @step(u'Doit afficher «([^»]*)(»?)')
 def doit_afficher(step, expected, singleline):
-    # Append first prompt:
-    world.process.expect('.*')
-    world.output += world.process.after
-    # Process user inputs:
-    for user_input in world.inputs:
-        world.process.sendline(user_input)
+    try:
+        user_input = None
+        # Append first prompt:
         world.process.expect('.*')
-        world.output += world.process.after  # we see input + result
-    # The end:
-    world.process.expect('.*')
-    # We rstrip because Lettuce seems to do the same when parsing multiline.
-    world.output += world.process.after.rstrip('\n')
-    world.process.expect(pexpect.EOF)
+        world.output += world.process.after
+        # Process user inputs:
+        for user_input in world.inputs:
+            world.process.sendline(user_input)
+            world.process.expect('.*')
+            world.output += world.process.after  # we see input + result, nice
+        # The end:
+        world.process.expect('.*')
+        # We rstrip because Lettuce seems to do the same when parsing multiline.
+        world.output += world.process.after.rstrip('\n')
+        world.process.expect(pexpect.EOF)
+    except KeyboardInterrupt:
+        print "Killing process before reraising KeyboardInterrupt."
+        world.process.close()
+        raise KeyboardInterrupt
+    except pexpect.EOF:
+        world.process.close()
+        if user_input is None:
+            tpl = "Le programme etait deja fini avant toute interaction.\n" + \
+                  "La sortie etait %s"
+            raise AssertionError(tpl % world.output)
+        else:
+            tpl = "Le programme etait deja fini lors de %s. La sortie etait %s"
+            raise AssertionError(tpl % (user_input, world.output))
+    except pexpect.TIMEOUT:
+        world.process.close()
+        tpl = "Le test dure + de %s s. La sortie etait %s"
+        raise AssertionError(tpl % (world.timeout, world.output))
     # Post process output:
     world.output = world.output.replace('\r', '')
     if not singleline:
