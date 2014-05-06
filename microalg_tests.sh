7ca01a178ae53a259929e2cdcfa16b58131e5292
Christophe Gragnic <christophegragnic@gmail.com>
2014-05-06 09:24:55 +0200

    Test des features inclus dans 'tous les tests'.

--- microalg_tests.sh
+++ microalg_tests.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-sh microalg_tests_picolisp.sh && sh microalg_tests_ersatz.sh && sh microalg_tests_emulisp.sh
-
+sh microalg_tests_picolisp.sh
+EXIT_MALG=$?
+sh microalg_tests_ersatz.sh
+EXIT_MALGJ=$?
+sh microalg_tests_emulisp.sh
+EXIT_MALGJS=$?
+sh microalg_tests_features.sh
+EXIT_FEATURES=$?
+exit $(($EXIT_MALG | $EXIT_MALGJ | $EXIT_MALGJS | $EXIT_FEATURES))
