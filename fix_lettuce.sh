e1a1790b191bb9f76a758bce9e0c6337a65173f4
Christophe Gragnic <christophegragnic@gmail.com>
2014-05-07 08:06:11 +0200

    Ajustements dans .travis.yml: fix lettuce.

--- fix_lettuce.sh
+++ fix_lettuce.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -i -e 's/self.wrt(str(reason.step))/self.wrt(unicode(reason.step))/' /home/travis/virtualenv/python2.7.6/lib/python2.7/site-packages/lettuce/plugins/reporter.py
+sed -i -e 's/self.wrt(str(reason.step))/self.wrt(unicode(reason.step))/' /usr/local/lib/python2.7/dist-packages/lettuce/plugins/reporter.py
