76d622668f7260686d6f1c5fa0c157798552f05c
Christophe Gragnic <christophegragnic@gmail.com>
2014-06-05 11:57:53 +0200

    Web IDE: Curseur en fin d'editor.

--- web/ide_injections.js
+++ web/ide_injections.js
@@ -110,3 +110,19 @@ function inject_microalg_repl_in(elt_id, msg) {
         old_src = repl_elt.val();
     }
 }
+
+http://www.sitepoint.com/jquery-set-focus-character-range/
+$.fn.selectRange = function(start, end) {
+    return this.each(function() {
+        if (this.setSelectionRange) {
+            this.focus();
+            this.setSelectionRange(start, end);
+        } else if (this.createTextRange) {
+            var range = this.createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', end);
+            range.moveStart('character', start);
+            range.select();
+        }
+    });
+};
