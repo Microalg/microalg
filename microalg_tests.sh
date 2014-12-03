6f5223ebc2bf6401307d00e9d122f165c059ba81
Christophe Gragnic <christophegragnic@gmail.com>
2014-12-03 16:04:21 +0100

    Résactivation des tests avec EmuLisp."
    This reverts commit c9dc8f52289619e018b392ad86718e19e5c118fb.

--- microalg_tests.sh
+++ microalg_tests.sh
@@ -3,6 +3,8 @@ sh microalg_tests_picolisp.sh
 EXIT_MALG=$?
 sh microalg_tests_ersatz.sh
 EXIT_MALGJ=$?
+sh microalg_tests_emulisp.sh
+EXIT_MALGJS=$?
 #sh microalg_tests_features.sh
 EXIT_FEATURES=0
-exit $(($EXIT_MALG | $EXIT_MALGJ | $EXIT_FEATURES))
+exit $(($EXIT_MALG | $EXIT_MALGJ | $EXIT_MALGJS | $EXIT_FEATURES))
