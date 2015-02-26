b973e5f1c6a7914dca84220b4bae6d9edba8c4a1
Christophe Gragnic <christophegragnic@gmail.com>
2015-02-26 21:38:18 +0100

    SciTE: ajout de -bye pour l’exécution sous Linux.

--- editeurs/scite/microalg.lua
+++ editeurs/scite/microalg.lua
@@ -52,9 +52,9 @@ props["abbreviations.*.malg"] = "$(microalg_path)/editeurs/scite/malg_abbrev.pro
 
 -- Configure F5 action
 if uname_s == "Linux" then
-        -- props["command.go.*.malg"] = "$(microalg_path)/picolisp/pil $(microalg_path)/microalg.l $(FilePath)"
-        props["command.go.*.malg"] = "$(microalg_path)/ersatz/pilj $(microalg_path)/microalg.l $(FilePath)"
-        -- props["command.go.*.malg"] = "$(microalg_path)/emulisp/piljs $(microalg_path)/microalg.l $(FilePath)"
+        -- props["command.go.*.malg"] = "$(microalg_path)/picolisp/pil $(microalg_path)/microalg.l $(FilePath) -bye"
+        props["command.go.*.malg"] = "$(microalg_path)/ersatz/pilj $(microalg_path)/microalg.l $(FilePath) -bye"
+        -- props["command.go.*.malg"] = "$(microalg_path)/emulisp/piljs $(microalg_path)/microalg.l $(FilePath) -bye"
 else
         props["command.go.*.malg"] = "$(SciteDefaultHome)\\malgj_scite.bat $(FilePath)"
 end
