138388fb21bcafcf4457a98c7396c63e06677297
Christophe Gragnic <christophegragnic@gmail.com>
2015-03-14 19:14:58 +0100

    Restructuration des scripts.

--- malg-njs.bat
+++ malg-njs.bat
@@ -4,5 +4,5 @@ set NODE_PATH=%USERPROFILE%\AppData\Roaming\npm\node_modules;"%NODE_PATH%"
 echo MicroAlg (EmuLisp)
 SET MICROALG_DIR=%~dp0
 SET EMULISP_DIR=%MICROALG_DIR%\emulisp
-node %EMULISP_DIR%\piljs %MICROALG_DIR%\microalg.l %1 %2 %3 %4 %5 %6
+node %EMULISP_DIR%\pil-njs %MICROALG_DIR%\microalg.l %1 %2 %3 %4 %5 %6
 pause>nul|set/p="Une touche pour quitter."&echo(
