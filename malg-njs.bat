5f81b73121e7852fb3362d5a58a0ac59769436e2
Christophe Gragnic <christophegragnic@gmail.com>
2015-03-14 23:07:47 +0100

    malg*: indication de la version.

--- malg-njs.bat
+++ malg-njs.bat
@@ -1,7 +1,7 @@
 @echo off
 REM https://github.com/Microalg/Microalg/blob/latest/INSTALL.md#avec-nodejs
 set NODE_PATH=%USERPROFILE%\AppData\Roaming\npm\node_modules;"%NODE_PATH%"
-echo MicroAlg (EmuLisp)
+echo MicroAlg (NodeJS + EmuLisp)
 SET MICROALG_DIR=%~dp0
 SET EMULISP_DIR=%MICROALG_DIR%\emulisp
 node %EMULISP_DIR%\pil-njs %MICROALG_DIR%\microalg.l %1 %2 %3 %4 %5 %6
