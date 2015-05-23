20918c34061c299a484ac6144579ecb46e0b8e4d
Christophe Gragnic <christophegragnic@gmail.com>
2015-05-23 09:03:21 +0200

    Utiliser 'echo.' plutot que 'echo' pour ligne vide dans les .bat.

--- malg-j.bat
+++ malg-j.bat
@@ -1,6 +1,6 @@
 @echo off
 echo MicroAlg 0.3.15 (Ersatz)
-echo
+echo.
 SET MICROALG_DIR=%~dp0
 SET ERSATZ_DIR=%MICROALG_DIR%\ersatz
 java -DPID=42 -jar %ERSATZ_DIR%\picolisp.jar %ERSATZ_DIR%\lib.l %MICROALG_DIR%microalg.l %1 -bye
--- malg-njs.bat
+++ malg-njs.bat
@@ -1,7 +1,7 @@
 @echo off
 set NODE_PATH=%USERPROFILE%\AppData\Roaming\npm\node_modules;"%NODE_PATH%"
 echo MicroAlg 0.3.15 (NodeJS + EmuLisp)
-echo
+echo.
 SET MICROALG_DIR=%~dp0
 SET EMULISP_DIR=%MICROALG_DIR%\emulisp
 node %EMULISP_DIR%\pil-njs %MICROALG_DIR%\microalg.l "%1" -bye
