66daa5d7ed2c00b1c3339c44d31ef58180c2c421
Christophe Gragnic <christophegragnic@gmail.com>
2015-06-10 08:40:50 +0200

    SciTE: fix escaped hat char handling with jrepl.

--- editeurs/scite/microalg.lua
+++ editeurs/scite/microalg.lua
@@ -101,7 +101,7 @@ else
         jrepl_opts = " /X"
         enclosing_quotes = jrepl ..    [["^\q(.*)\q$" $1]] .. jrepl_opts
         escaped_dble_quotes = jrepl .. [["\\\q" "\q"]] .. jrepl_opts
-        escaped_hat_char = jrepl ..    [["\\\^" "\^"]] .. jrepl_opts
+        escaped_hat_char = jrepl ..    [["\\\^" "^"]] .. jrepl_opts
         escaped_backslash = jrepl ..   [["\\\\" "\"]] .. jrepl_opts
         hat_J = jrepl ..               [["\^J" "\n"]] .. jrepl_opts
         props["command.go.*.malg"] = [[$(SciteDefaultHome)\$(1)-scite.bat $(FilePath)]]
