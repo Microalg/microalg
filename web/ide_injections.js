f9d17276644ec26af7ac92ad2c4f8d874e407dc8
Christophe Gragnic <christophegragnic@gmail.com>
2015-08-19 16:49:45 +0200

    Blockly: suppression du bloc Initialiser de la toolbox.

--- web/ide_injections.js
+++ web/ide_injections.js
@@ -311,7 +311,6 @@ function inject_microalg_editor_in(elt_id, config) {
                 '  <block type="commentaire"></block>' +
                 '  <block type="afficher"></block>' +
                 '  <block type="declarer"></block>' +
-                '  <block type="initialiser"></block>' +
                 '  <block type="affecter_a"></block>' +
                 '  <block type="initialiser_pseudo_aleatoire"></block>' +
                 '  <block type="si"></block>' +
