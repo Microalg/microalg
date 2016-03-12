9826324ba9962fac2f9111e938c1d273fcfb1490
Christophe Gragnic <christophegragnic@gmail.com>
2016-03-12 08:06:53 +0100

    EmuLisp: better error msg for xmlhttpreq

--- emulisp/emulisp_core.js
+++ emulisp/emulisp_core.js
@@ -30,7 +30,7 @@ function getFileSync(fileUrl) {
 		}
 		return req.responseText;
 	}
-	throw new Error("XMLHttpRequest status: " + req.status);
+	throw new Error("Retrieving " + fileUrl + ": XMLHttpRequest status " + req.status);
 }
 
 var NILTYPE = 0, NUMBERTYPE = 1, SYMBOLTYPE = 2, CELLTYPE = 3, TRUETYPE = 4;
